8050783dd8cb4b52ce7f10b6b10fc85a8aa3eaa3
Vladislav <vladislav33lavrov@gmail.com>
2022-12-13 21:32:27 +0300

    add exercise_3

---
+++ exercise_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("это выходной день");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("такого дня недели нет");
+  }
+  else Console.WriteLine("это не выходной день");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
