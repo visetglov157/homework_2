5e68dd33c91bacf59b5168c52add162230087a35
Vladislav <vladislav33lavrov@gmail.com>
2022-12-13 21:07:06 +0300

    add exercise 1 and 2

---
+++ exercise_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введи трёхзначное число: ");
+int num= Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(num);
+Console.WriteLine("вторая цифра числа - "+stringNumber[1]);
---
+++ exercise_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра - " + anyNumberText[2]);
+}
+else {
+  Console.Write("третьей цифры нет");
+}
